a2883201d0d12fba3e8b9f1c0b0dc93d20c6819f
lightomega <lightomega36@gmail.com>
2021-10-05 19:22:54 +0200

    inscription/connexion
    mise à jour des utilisateurs et de l'authentification, probleme de token à résoudre

--- back/app.js
+++ back/app.js
@@ -2,21 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
 
-const app = express() 
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    next()
-})
-
-mongoose.connect('mongodb+srv://Alexandre:ocdzkBM9R3SR3LZK@cluster0.arxda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://Alexandre:UKzRkttQ8IINTlVx@cluster0.arxda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     { useNewUrlParser: true,
     useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'))
 
+const app = express() 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use(express.json())
 
