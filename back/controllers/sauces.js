d0cae131314959a4e78ea1aeeb12e5917516a377
lightomega <lightomega36@gmail.com>
2021-11-07 14:58:35 +0100

    js controller
    ajout d'une fonction pour supprimer les images lors de la suppression d'une sauce

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -1,5 +1,6 @@
 //récupération du model sauces
 const Sauce = require("../models/sauces");
+const fs = require("fs");
 
 //controller qui va créer une sauce si le model reçu par le front est conforme au model attendu par le back
 exports.createSauces = (req, res, next) => {
@@ -27,17 +28,26 @@ exports.modifySauces = (req, res, next) => {
         }`,
       }
     : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id, userId: sauceObject.userId },
-    { ...sauceObject })
+  Sauce.updateOne(
+    { _id: req.params.id, userId: sauceObject.userId },
+    { ...sauceObject }
+  )
     .then(() => res.status(200).json({ message: "Objet modifié !" }))
     .catch((error) => res.status(400).json({ error }));
 };
 
 //controller qui va permettre à l'utilisateur de supprimer la sauce qu'il a ajouté
 exports.deleteSauces = (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 //controller qui affiche la sauce selectionnée
